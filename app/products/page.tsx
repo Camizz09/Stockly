20b202ea75bf8dcbe8bae1afc45bea6304178607
camilladavyla <lendaukoro12games@gmail.com>
2025-04-17 22:41:15 -0300

    feat: add initial products dialog

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,7 @@
-import { PlusIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import { DataTable } from "../_components/ui/data-table";
 import { productTableColumns } from "./_components/table-columns";
 import { getProducts } from "../_data-acess/product/get-product";
+import AddProductButton from "./_components/add-product-button";
 
 export const revalidate = 10;
 
@@ -17,10 +16,7 @@ const ProductsPage = async () => {
           </span>
           <h2 className="text-xl font-semibold">Produtos</h2>
         </div>
-        <Button className="gap-2">
-          <PlusIcon size={20} />
-          Novo produto
-        </Button>
+        <AddProductButton />
       </div>
       <DataTable columns={productTableColumns} data={products} />
     </div>

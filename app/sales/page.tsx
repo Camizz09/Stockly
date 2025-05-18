04dbd82ecf8a7b244fe8d49bacbb2586429ff019
camilladavyla <lendaukoro12games@gmail.com>
2025-05-17 22:54:39 -0300

    chore: add foreign keys to sale and product table

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,8 +1,7 @@
-import { Button } from "../_components/ui/button";
 import { ComboboxOption } from "../_components/ui/combobox";
-import { Sheet, SheetTrigger } from "../_components/ui/sheet";
 import { getProducts } from "../_data-acess/product/get-product";
-import UpsertSheetContent from "./_components/upsert-sheet-content";
+import CreateProductButton from "../products/_components/create-product-button";
+import CreateSaleButton from "./_components/create-sale-button";
 
 const SalesPage = async () => {
   const products = await getProducts();
@@ -19,15 +18,7 @@ const SalesPage = async () => {
           </span>
           <h2 className="text-xl font-semibold">Vendas</h2>
         </div>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button>Nova Venda</Button>
-          </SheetTrigger>
-          <UpsertSheetContent
-            products={products}
-            productOptions={productOptions}
-          />
-        </Sheet>
+        <CreateSaleButton products={products} productOptions={productOptions} />
       </div>
       {/* <DataTable columns={productTableColumns} data={products} /> */}
     </div>

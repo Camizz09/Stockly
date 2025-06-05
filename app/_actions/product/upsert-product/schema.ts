4efc04cdec1c0f91b39de7c681a55f9ecea5851e
Atauan <94688135+Atauan@users.noreply.github.com>
2025-06-05 00:09:45 -0300

    Melhorias no Sistema Stockly

--- app/_actions/product/upsert-product/schema.ts
+++ app/_actions/product/upsert-product/schema.ts
@@ -2,15 +2,9 @@ import { z } from "zod";
 
 export const upsertProductSchema = z.object({
   id: z.string().uuid().optional(),
-  name: z.string().trim().min(1, {
-    message: "O nome do produto é obrigatório",
-  }),
-  price: z.number().min(0.01, {
-    message: "O preço do produto é obrigatório",
-  }),
-  stock: z.coerce.number().int().min(0, {
-    message: "A quantidade em estoque é obrigatória.",
-  }),
+  name: z.string().min(1, "Nome é obrigatório"),
+  price: z.number().positive("Preço deve ser maior que zero"),
+  stock: z.number().int().nonnegative("Estoque não pode ser negativo"),
 });
 
 export type UpsertProductSchema = z.infer<typeof upsertProductSchema>;
